9264bb8cf088dff94e9aed439c3a0a07076a847e
funnysunny08 <rosa7536@naver.com>
2022-11-18 23:58:00 +0900

    [feature/#4] SearchRouter의 router url 수정

--- src/router/SearchRouter.ts
+++ src/router/SearchRouter.ts
@@ -4,6 +4,6 @@ import { SearchController } from "../controller";
 const router: Router = Router();
 
 //* 검색 결과 가져오기
-router.get("/search", SearchController.getSearchData);
+router.get("/", SearchController.getSearchData);
 
 export default router;
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import userRouter from "./userRouter";
+import SearchRouter from "./SearchRouter";
 
 const router: Router = Router();
 
 router.use("/user", userRouter);
+router.use("/search", SearchRouter);
 
 export default router;
