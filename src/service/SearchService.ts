2ba550bb12837c472077b1c1136bd34aef603771
funnysunny08 <rosa7536@naver.com>
2022-11-21 18:45:06 +0900

    [feature/#4] SearchService 추가중

--- src/service/SearchService.ts
+++ src/service/SearchService.ts
@@ -2,7 +2,13 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 const getSearchData = async (name: string) => {
-  const data = await prisma.product.findMany()
+  const brandList = await prisma.brand.findMany();
+  const productList = await prisma.product.findMany();
+  JSON.stringify(brandList);
+  JSON.stringify(productList);
+
+  const data: object = {"brands": brandList, "products": productList};
+
   return data;
 };
 
