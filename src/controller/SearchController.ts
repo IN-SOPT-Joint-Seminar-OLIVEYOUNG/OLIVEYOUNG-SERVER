c3eba21fa80f8c1a47132a96ee7e1a9a0c5ee0d5
dltjdn <hana40004@naver.com>
2022-11-23 05:10:14 +0900

    [feature/#3] 메인 검색 화면 API

---
+++ src/controller/SearchController.ts
@@ -0,0 +1,26 @@
+import { Request, Response } from "express";
+import { rm, sc } from "../constants";
+import { success, fail } from "../constants/response";
+import SearchService from "../service/SearchService";
+
+//* 추천 상품 조회
+const getSearchMain = async (req: Request, res: Response) => {
+  try {
+    const data = await SearchService.getSearchMain();
+
+    if (!data) {
+      return res.status(sc.NOT_FOUND).send(fail(sc.NOT_FOUND, rm.NOT_FOUND));
+    }
+    return res.status(sc.OK).send(success(sc.OK, rm.GET_SEARCH_SUCCESS, data));
+  } catch (error) {
+    return res
+      .status(sc.INTERNAL_SERVER_ERROR)
+      .send(fail(sc.INTERNAL_SERVER_ERROR, rm.INTERNAL_SERVER_ERROR));
+  }
+};
+
+const SearchController = {
+  getSearchMain,
+};
+
+export default SearchController;
