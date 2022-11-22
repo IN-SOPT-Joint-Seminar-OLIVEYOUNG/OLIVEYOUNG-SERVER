453395c2969d4a283372d9eb7cac87a4d3211bcc
dltjdn <hana40004@naver.com>
2022-11-23 04:43:44 +0900

    [feature/#2] 브랜드별 상품조회 API

--- src/controller/MainController.ts
+++ src/controller/MainController.ts
@@ -3,6 +3,7 @@ import { rm, sc } from "../constants";
 import { success, fail } from "../constants/response";
 import { MainService } from "../service";
 
+//* 추천 상품 조회
 const getPickedProduct = async (req: Request, res: Response) => {
   try {
     const data = await MainService.getPickedProduct();
@@ -10,9 +11,25 @@ const getPickedProduct = async (req: Request, res: Response) => {
     if (!data) {
       return res.status(sc.NOT_FOUND).send(fail(sc.NOT_FOUND, rm.NOT_FOUND));
     }
+    return res.status(sc.OK).send(success(sc.OK, rm.GET_PICKED_SUCCESS, data));
+  } catch (error) {
     return res
-      .status(sc.CREATED)
-      .send(success(sc.CREATED, rm.GET_PICKED_SUCCESS, data));
+      .status(sc.INTERNAL_SERVER_ERROR)
+      .send(fail(sc.INTERNAL_SERVER_ERROR, rm.INTERNAL_SERVER_ERROR));
+  }
+};
+
+//* 브랜드 별 상품 조회
+const getBrandProduct = async (req: Request, res: Response) => {
+  try {
+    const data = await MainService.getBrandProduct();
+
+    if (!data) {
+      return res.status(sc.NOT_FOUND).send(fail(sc.NOT_FOUND, rm.NOT_FOUND));
+    }
+    return res
+      .status(sc.OK)
+      .send(success(sc.OK, rm.GET_BRAND_PRODUCT_SUCCESS, data));
   } catch (error) {
     return res
       .status(sc.INTERNAL_SERVER_ERROR)
@@ -22,6 +39,7 @@ const getPickedProduct = async (req: Request, res: Response) => {
 
 const MainController = {
   getPickedProduct,
+  getBrandProduct,
 };
 
 export default MainController;
